9b4e3546e7e333009750e979ac38c23e5f86c209
root <root@ip-10-0-0-151.ap-northeast-2.compute.internal>
2025-08-26 09:40:05 +0000

    Update app.py message to v2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from soldesk"
+    return "Hello from v2!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
