3943648250067bceed193ba417c11aed38016878
root <root@ip-10-0-0-151.ap-northeast-2.compute.internal>
2025-08-26 09:44:11 +0000

    Update app.py: show hostname for load-balancing test

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import socket   # 호스트네임 가져오기용
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from v2!"
+    hostname = socket.gethostname()
+    return f"Hello from soloud! (pod: {hostname})"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
